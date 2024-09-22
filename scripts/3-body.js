7e624bb4f66848b6294f045ac143610c2df5f549
Albert Hovda Røed <albertroed@hotmail.com>
2024-09-22 19:35:15 +0200

    new Simple timing loop

--- scripts/3-body.js
+++ scripts/3-body.js
@@ -1,16 +1,17 @@
 let canvas = document.getElementById("3body");
 let ctx = canvas.getContext("2d");
 
-let width = parseInt(canvas.getAttribute("width"));
-let height = parseInt(canvas.getAttribute("height"));
+let w = parseInt(canvas.getAttribute("width"));
+let h = parseInt(canvas.getAttribute("height"));
 
 class Planet {
     // Properties
-    constructor (canvas_width,canvas_height,memory,maxmass,color) {
-        this.x = Math.random()*canvas_width/2+canvas_width/4;
-        this.y = Math.random()*canvas_height/2+canvas_height/4;
+    constructor (memory,maxmass,color) {
+        // TODO: make a circular start in stead of a square start
+        this.x = Math.random()*w/2+w/4;
+        this.y = Math.random()*h/2+h/4;
         this.angle = Math.random()*2*Math.PI;
-        this.velocity = (canvas_width+canvas_height)/10*Math.random();
+        this.velocity = (w+h)/100*Math.random();
         this.history = [];
         this.historylen = memory;
         this.mass = Math.random()*maxmass;
@@ -18,23 +19,59 @@ class Planet {
         this.r_gradient = 50;
         this.color = color;
     }
-    draw(ctx) {
-        console.log(this.color);
+    draw() {
+        //TODO: make gradient from this.color to black
+        ctx.beginPath();
         ctx.fillStyle = this.color;
-        ctx.arc(this.x,this.y,this.r,0,2*Math.PI)
+        ctx.arc(this.x, this.y, this.r, 0, 2*Math.PI);
         ctx.fill();
+        // Draw speed vector
+        ctx.beginPath();
+        ctx.strokeStyle = "white";
+        ctx.moveTo(this.x,this.y);
+        let vscale = 10;
+        ctx.lineTo(this.x+Math.cos(this.angle)*this.velocity*vscale,
+                   this.y+Math.sin(this.angle)*this.velocity*vscale);
+        ctx.stroke();
+    }
+    update() {
+        
     }
 }
-function draw(ctx,w,h,planets) {
+
+let planet1 = new Planet(100,300,"red");
+let planet2 = new Planet(100,300,"blue");
+let planets = [planet1, planet2];
+
+// create n planets
+for (let i = 0; i < 5; i++) {
+    planets.push(new Planet(100,300,"yellow"));
+}
+
+function update() {
+
+}
+
+function draw() {
     ctx.fillStyle = "black";
     ctx.fillRect(0,0,w,h);
     ctx.fill();
-    ctx.fillStyle = "blue";
     for (i = 0; i<planets.length;i++) {
         planets[i].draw(ctx);
     }
 }
-let planet1 = new Planet(width,height,100,300,"red");
-let planet2 = new Planet(width,height,100,300,"orange");
-let planets = [planet1, planet2];
-draw(ctx,width,height,planets);
+
+let looptest = 0;
+let total_time = 0;
+let lastframetime = 0;
+function loop(time) {
+    if (time >= lastframetime + 100) { // check if 100ms has passed
+        looptest++;
+        document.getElementById("looptest").innerHTML = looptest;
+        lastframetime = time;
+    }
+    draw();
+    requestAnimationFrame(loop);
+}
+
+requestAnimationFrame(loop);
