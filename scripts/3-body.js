ebb81ff9aa4dd802e589098f9a077702ed1457b6
Albert Hovda Røed <albertroed@hotmail.com>
2024-09-23 22:04:36 +0200

    new planets copy

--- scripts/3-body.js
+++ scripts/3-body.js
@@ -10,11 +10,23 @@ class Planet {
     // Properties
     constructor (memory,maxmass,color,x=undefined,y=undefined,v=undefined) {
         // TODO: make a circular start in stead of a square start
-        this.x = Math.random()*w/2+w/4;
-        this.y = Math.random()*h/2+h/4;
+        if (typeof(x) == "undefined") {
+            this.x = Math.random()*w/2+w/4;
+        } else {
+            this.x = x
+        }
+        if (typeof(y) == "undefined") {
+            this.y = Math.random()*h/2+h/4;
+        } else {
+            this.y = y
+        }
         let angle = Math.random()*2*Math.PI;
         let velocity = (w+h)/50*Math.random();
-        this.velocity = [velocity*Math.cos(angle),velocity*Math.sin(angle)];
+        if (typeof(v) == "undefined") {
+            this.velocity = [velocity*Math.cos(angle),velocity*Math.sin(angle)];
+        } else {
+            this.velocity = v;
+        }
         this.history = [[0,0]];
         this.historylen = memory;
         this.mass = 100 //Math.random()*maxmass;
@@ -93,6 +105,7 @@ class Planet {
     }
 }
 
+// create planets
 let planet1 = new Planet(100,300,"red");
 let planet2 = new Planet(100,300,"blue");
 let planets = [planet1, planet2];
@@ -102,7 +115,17 @@ for (let i = 0; i < 1; i++) {
     planets.push(new Planet(100,300,"yellow"));
 }
 
-function update() {
+// create planet copies
+let planetcopies = [];
+for (let i = 0; i < planets.length; i++) {
+    if (i == 0) {
+        planetcopies.push(new Planet(100,planets[i].mass,planets[i].color,planets[i].x+1,planets[i].y,planets[i].velocity))
+    } else {
+        planetcopies.push(new Planet(100,planets[i].mass,planets[i].color,planets[i].x,planets[i].y,planets[i].velocity))
+    }
+}
+
+function update(planets) {
     for (i = 0; i < planets.length; i++) {
         let otherplanets = [];
         for (let ii = 0; ii < planets.length; ii++) {
@@ -113,6 +136,7 @@ function update() {
         }
         planets[i].update(otherplanets);
     }
+    // TODO: Add camera zooming
     let averagecord = [0,0];
     for (let i = 0; i < planets.length; i++) {
         planets[i].update_lastpos()
@@ -124,10 +148,7 @@ function update() {
     camcords = averagecord;
 }
 
-function draw() {
-    ctx.fillStyle = "black";
-    ctx.fillRect(0,0,w,h);
-    ctx.fill();
+function draw(planets) {
     for (i = 0; i<planets.length;i++) {
         planets[i].drawhistory(ctx); 
     }
@@ -145,11 +166,14 @@ function loop(time) {
         looptest++;
         document.getElementById("looptest").innerHTML = looptest;
         lastframetime = time;
-        update();
+        update(planets);
+        update(planetcopies);
     }
-    draw();
+    ctx.fillStyle = "black";
+    ctx.fillRect(0,0,w,h);
+    ctx.fill();
+    draw(planets);
+    draw(planetcopies);
     requestAnimationFrame(loop);
 }
-//update();
-//draw();
 requestAnimationFrame(loop);
